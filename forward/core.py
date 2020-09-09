7206be368d54e7f4aeb6c0c67779fa60ba66f84e
skbaji6 <1@Samreenayaz>
2020-09-09 06:23:45 +0530

    informative message change and start command

--- forward/core.py
+++ forward/core.py
@@ -68,10 +68,10 @@ async def help(event):
 @bot.on(events.NewMessage(pattern='/start'))
 async def help(event):
     await event.respond("Step 1: Add this Bot to your group or Channel as admin\n"
-                        "Step 2: Get ids from channel or group using /getid command from respective channel or group "
-                        "Step 3: By using those ids configure forwards using following Commands"
-                        "\n /setforward source-channel-id destination-channel-id (Ex:/setforward 123456 789101)"
-                        "\n /removeforward source-channel-id (Ex:/setforward 123456)")
+                        "\nStep 2: Get ids from channel or group using /getid command from respective channel or group "
+                        "\n\nStep 3: By using those ids configure forwards using following Commands"
+                        "\n\n /setforward source-channel-id destination-channel-id (Ex:/setforward 123456 789101)"
+                        "\n\n /removeforward source-channel-id (Ex:/setforward 123456)")
     raise events.StopPropagation
 
 
