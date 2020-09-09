8063c054384aaac5be86d81fcaef93469c9c7f27
skbaji6 <1@Samreenayaz>
2020-09-09 23:10:45 +0530

    remove stickers from forward

--- forward/core.py
+++ forward/core.py
@@ -21,22 +21,26 @@ mongo_client = pymongo.MongoClient(Config.MONGO_DB_URL)
 db = mongo_client.samaydb
 forward_map = dict()
 
+
 def update_forward_map():
     global forward_map
     for forid in list(db.forwards.find({})):
         forward_map[forid['_id']] = forid['forwardTo']
 
+
 update_forward_map()
 
-@bot.on(events.NewMessage(pattern='/setforward',from_users=list(Config.SUDO_USERS)))
+
+@bot.on(events.NewMessage(pattern='/setforward', from_users=list(Config.SUDO_USERS)))
 async def seforward(event):
     command = event.text.split()
-    if len(command) > 2 :
-        db.forwards.find_one_and_update({'_id': command[1]}, {'$set': {'_id': command[1], 'forwardTo': command[2]}}, upsert=True)
+    if len(command) > 2:
+        db.forwards.find_one_and_update({'_id': command[1]}, {'$set': {'_id': command[1], 'forwardTo': command[2]}},
+                                        upsert=True)
         update_forward_map()
         await event.respond(f"Forward set {command[1]} ==> {command[2]}")
         raise events.StopPropagation
-    else :
+    else:
         await event.respond("Please Enter Source and Destination chat Ids Ex:/setforward 123456 789101")
 
 
@@ -45,7 +49,8 @@ async def getid(event):
     await event.respond(f'{event.message.chat_id}')
     raise events.StopPropagation
 
-@bot.on(events.NewMessage(pattern='/removeforward',from_users=list(Config.SUDO_USERS)))
+
+@bot.on(events.NewMessage(pattern='/removeforward', from_users=list(Config.SUDO_USERS)))
 async def removeforward(event):
     command = event.text.split()
     if len(command) > 1:
@@ -56,6 +61,7 @@ async def removeforward(event):
         await event.respond("Please Enter chat id to remove forward Ex:/removeforward 123456")
     raise events.StopPropagation
 
+
 @bot.on(events.NewMessage(pattern='/help'))
 async def help(event):
     await event.respond("Step 1: Add this Bot to your group or Channel as admin\n"
@@ -64,6 +70,7 @@ async def help(event):
                         "\n\n /setforward source-channel-id destination-channel-id (Ex:/setforward 123456 789101)"
                         "\n\n /removeforward source-channel-id (Ex:/removeforward 123456)")
     raise events.StopPropagation
+
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def help(event):
@@ -77,8 +84,9 @@ async def help(event):
 
 @bot.on(events.NewMessage)
 async def echo(event):
-    #user = await event.client.get_entity(-1001297647039)
-    if  event.message.document is not None or event.message.video is not None and f'{event.message.chat_id}' in forward_map :
+    # user = await event.client.get_entity(-1001297647039)
+    if (event.message.document is not None and event.message.sticker is None) \
+            or event.message.video is not None and f'{event.message.chat_id}' in forward_map:
         user = await event.client.get_entity(int(forward_map[f'{event.message.chat_id}']))
         print(f">> get it {event.text}")
         await event.client.send_message(user, event.message)
